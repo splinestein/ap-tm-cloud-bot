c6e9967adb9c29b84d36889407fac6375eb255f8
Anthon98 <anthon.kallankari@gmail.com>
2020-05-16 16:43:56 +0300

    red tm

--- aptmdisbot.py
+++ aptmdisbot.py
@@ -3,15 +3,19 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = '-------')
 
+#tm ally sign #711211043212492880
+#red ally sign #711210923192352840
+
 @client.event
 async def on_message(message):
 
-    if message.channel.id == 710945711449178213:
-        #if msg from tm, send to ap sign in channel or whatever tm sign in channel they will have.
-        channel = client.get_channel(710946067549782146)
+    if message.channel.id == 701673305647743006:
+        #if msg from tm, send to red sign in channel or whatever tm sign in channel they will have.
+        #tm ally sign in channel #711211043212492880
+        channel = client.get_channel(711211043212492880)
         await channel.send(str(message.content))
-    if message.channel.id == 710945675357061254:
-        channel = client.get_channel(710946203545632811)
+    if message.channel.id == 692990720113639434:
+        channel = client.get_channel(711210923192352840)
         await channel.send(str(message.content))
 
 client.run('NzEwOTM4ODQwOTIzMjQyNTI3.Xr7xzw.rlIQdTmKnifHE4Se2kd_pHOMwzU')
