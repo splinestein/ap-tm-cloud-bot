8b6b7db0dd07f2169f57068f3513ae2c75420352
Anthon98 <anthon.kallankari@gmail.com>
2020-05-16 21:08:41 +0300

    everyyyything v2

--- aptmdisbot.py
+++ aptmdisbot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 
 client = commands.Bot(command_prefix = '-------')
 
@@ -16,16 +17,21 @@ client = commands.Bot(command_prefix = '-------')
 async def on_message(message):
 #692990720113639434
 
+now = datetime.now()
+
+dt_string = now.strftime("%H:%M:%S")
+
+
 #when sign in comes from red
     if message.channel.id == 701673305647743006:
 
         #Send it to TM.
         channel = client.get_channel(711211043212492880)
-        await channel.send(str("[RED] -> : ")+str(message.content)+" | Discord User : "+str(message.author))
+        await channel.send(str("[RED] -> : ")+str(message.content)+" / discord: "+str(message.author)+" / @ -> "+str(dt_string))
 
         #Send it to AP.
         channel = client.get_channel(711271480041668698)
-        await channel.send(str("[RED] -> : ")+str(message.content)+" | Discord User : "+str(message.author))
+        await channel.send(str("[RED] -> : ")+str(message.content)+" / discord: "+str(message.author)+" / @ -> "+str(dt_string))
 
 
 #Aps sign in
@@ -33,11 +39,11 @@ async def on_message(message):
 
         #Send it to RED
         channel = client.get_channel(711210923192352840)
-        await channel.send(str("[AP] -> : ")+str(message.content)+" | Discord User : "+str(message.author))
+        await channel.send(str("[AP] -> : ")+str(message.content)+" / discord: "+str(message.author)+" / @ -> "+str(dt_string))
 
         #Send it to TM.
         channel = client.get_channel(711211043212492880)
-        await channel.send(str("[AP] -> : ")+str(message.content)+" | Discord User : "+str(message.author))
+        await channel.send(str("[AP] -> : ")+str(message.content)+" / discord: "+str(message.author)+" / @ -> "+str(dt_string))
 
 
 #TM sign in
@@ -45,11 +51,11 @@ async def on_message(message):
 
         #Send it to AP
         channel = client.get_channel(711271480041668698)
-        await channel.send(str("[TM] -> : ")+str(message.content)+" | Discord User : "+str(message.author))
+        await channel.send(str("[TM] -> : ")+str(message.content)+" / discord: "+str(message.author)+" / @ -> "+str(dt_string))
 
         #Send it to RED
         channel = client.get_channel(711210923192352840)
-        await channel.send(str("[TM] -> : ")+str(message.content)+" | Discord User : "+str(message.author))
+        await channel.send(str("[TM] -> : ")+str(message.content)+" / discord: "+str(message.author)+" / @ -> "+str(dt_string))
 
 
 client.run('NzEwOTM4ODQwOTIzMjQyNTI3.Xr7xzw.rlIQdTmKnifHE4Se2kd_pHOMwzU')
